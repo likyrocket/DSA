39e8c7fe29fa294df00b0f8f4a9d462083c29f6a
likyrocket <likyrocket@gmail.com>
2024-07-30 00:28:17 -0400

    bitonic code revised

--- demo/src/main/java/com/example/Bitonic.java
+++ demo/src/main/java/com/example/Bitonic.java
@@ -1,11 +1,11 @@
 package com.example;
-
+//test
 public class Bitonic {
     int findMaximum(int[] arr, int size) {
         int left=0,right=size-1,mid;
         while(left<=right){
             mid=left+(right-left)/2;
-            if((mid==0 || arr[mid]>arr[mid-1]) && (mid==size-1 || arr[mid]>arr[mid-1])){
+            if((mid==0 || arr[mid]>arr[mid-1]) && (mid==size-1 || arr[mid]>arr[mid+1])){
                 return arr[mid];
                 }
             else if(mid<size-1 && arr[mid]<arr[mid+1]){
