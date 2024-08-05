819bdb51e0ed7b2b397a080626fca26c5b5bf8c7
likyrocket <likyrocket@gmail.com>
2024-08-05 02:38:04 -0400

    summed matrix

---
+++ demo/src/main/java/com/example/SummedMatrix.java
@@ -0,0 +1,25 @@
+package com.example;
+
+public class SummedMatrix {
+    static long sumMatrix(long n, long q) {
+        // code here
+          long l=0;
+        
+        if(q==1 || q>(n+n)){
+            return l; // edge cases
+        }
+        
+        if(q<n+1){
+            l = q-1; //n-n+q-1
+            return l;
+        }
+        else if(q>=n+1){
+            l = n+n-q+1;
+            return l;
+        }
+        
+        return l;
+    }
+}
+    
+
