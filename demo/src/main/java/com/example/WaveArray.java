2cd6c6bc279d64708d1bfb53e8e1566efae3d936
likyrocket <likyrocket@gmail.com>
2024-08-07 05:09:33 -0400

    wave arrayy

--- demo/src/main/java/com/example/WaveArray.java
+++ demo/src/main/java/com/example/WaveArray.java
@@ -1,13 +1,31 @@
 package com.example;
 
 public class WaveArray {
-    public static void convertToWave(int n, int[] arr) {
+    public void convertToWave(int n, int[] arr) {
         // code here
-        for(int i = 1; i < n; i += 2) {
-            // Swap arr[i] and arr[i-1]
+        for (int i = 1; i < n; i += 2) {
             int temp = arr[i];
-            arr[i] = arr[i-1];
-            arr[i-1] = temp;
+            arr[i] = arr[i - 1];
+            arr[i - 1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println("Original array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        WaveArray solution = new WaveArray();
+        solution.convertToWave(n, arr);
+
+        System.out.println("Wave array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
